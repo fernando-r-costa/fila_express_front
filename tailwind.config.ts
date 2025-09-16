5a00a432c76becb9ce9b8e46e7ea772815fb1a8b
fernando-r-costa <fernando.r.costa@outlook.com>
2025-09-16 15:08:13 -0300

    feat: enviroment configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#B4CAC1',
+        'secondary': '#606060',
+        'black': '#000000',
+        'white': '#FFFFFF',
+      },
+      fontFamily: {
+        sans: ['var(--font-sqindra)', ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        'texture': "url('/textures/concrete.jpg')",
+      },
+    },
+  },
+  plugins: [
+    forms,
+  ],
+};
+
+export default config;
