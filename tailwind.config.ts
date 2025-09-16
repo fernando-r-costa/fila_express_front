0256d4f7fb79f06829db71ed0541b8c324324dd4
fernando-r-costa <fernando.r.costa@outlook.com>
2025-09-16 16:35:20 -0300

    fix: some tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss';
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import tailwindcssAnimate from 'tailwindcss-animate';
 
 const config: Config = {
   content: [
@@ -11,22 +12,20 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        'primary': '#B4CAC1',
-        'secondary': '#606060',
-        'black': '#000000',
-        'white': '#FFFFFF',
+        primary: '#B4CAC1',
+        secondary: '#606060',
+        black: '#000000',
+        white: '#FFFFFF',
       },
       fontFamily: {
         sans: ['var(--font-sqindra)', ...defaultTheme.fontFamily.sans],
       },
       backgroundImage: {
-        'texture': "url('/textures/concrete.jpg')",
+        texture: "url('/textures/concrete.jpg')",
       },
     },
   },
-  plugins: [
-    forms,
-  ],
+  plugins: [forms, tailwindcssAnimate],
 };
 
 export default config;
