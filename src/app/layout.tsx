c79167056d1aad6111e00e4f9ce4efea4886d8a1
fernando-r-costa <fernando.r.costa@outlook.com>
2025-10-04 10:34:00 -0300

    style(layout): adjust background image to cover screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br" className={`${sqindra.variable} scroll-smooth`}>
-      <body className="flex min-h-0 flex-1 flex-col bg-foreground bg-texture">
+      <body className="flex min-h-0 flex-1 flex-col bg-foreground bg-texture bg-cover bg-center bg-no-repeat">
         <Header />
         <Suspense
           fallback={
